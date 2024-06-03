945dc2a8cd176f5dc4e1af3a607f3a2095e3be25
mustafa-almomani <almomani363@gmail.com>
2024-06-04 00:41:04 +0300

    task1-javascript

---
+++ script.js
@@ -0,0 +1,79 @@
+let x = 250;
+console.log(250*0.025)
+// ////////////////////////////////////////////////////////////////
+//[ 1,7  9  45, ] this is after 
+let arr=[1,7,9,45]
+console.log(arr)
+//hint :add comma between elemant
+
+///["Str" "alex","moh"   this is after 
+let name = ["Str", "alex","moh" ]
+console.log(name )
+//hint :add close bracket and add comma 
+ 
+
+
+// 'the','fox' 'over' lazy, 'dog',  ]
+let animal =['the','fox' ,'over', 'lazy', 'dog' ]
+
+console.log(animal)
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+//What the index of "Banana","Tomato"
+//var fruits=["Tomato","Banana","Watermelon"]
+
+var fruits=["Tomato","Banana","Watermelon"]
+console.log("index of Tomato ="+fruits.indexOf("Tomato")+" \n index of banana ="+fruits.indexOf("Banana") )
+
+
+
+////////////////////////////////////////////////////
+let food=["mansaf","kabseh"]
+console.log("Favorite Food : "+food)
+let sport =["swiming", "gym" , "football"]
+console.log("Favorite sport : "+sport)
+let move=["who am I ","saw"]
+console.log("Favorite Food : "+move)
+///////////////////////////////////////////////////////
+let firstofarray=['m','u','s','t','a','f','a']
+console.log("first of array : "+firstofarray[0])
+
+/////////////////////////////////////////////////////
+let lastofarray=['m','u','s','t','a','f','a']
+console.log("last of array : "+lastofarray[lastofarray.length-1])
+//////////////////////////////////////////////////////////////////
+var array = [0,5,7,9]
+//[1,3,4,6,8,9,10]
+array.shift()
+array.shift()
+array.shift()
+
+console.log(array)
+array.unshift(1,3,4,6,8)
+array.push(10)
+console.log(array)
+
+
+///////////////////////////////////////////////////////////////////
+
+var array2 = [5,9,-7,3.5]
+array2.push("ali")
+console.log(array2)
+array2.unshift("mohamad",17)
+array2.pop()
+array2.shift()
+/////////////////////////////////////////////////////////////////
+
+
+
+var arr1 = [ -3, 8, 7, 6, 5, -4, 3, 2, 1 ];
+var arr2=arr1.sort()
+
+
+console.log(arr2)
+
+
+ 
